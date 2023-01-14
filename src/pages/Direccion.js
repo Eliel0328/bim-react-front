ff739859988583f4d7ca68ac3a5393ef076cda47
eliel.lopez <eliellopezfelipe0328@gmail.com>
2023-01-14 02:33:53 -0600

    Enviar los datos de la direccion Falta: Enviar los datos del usuario junto a la direccion

--- src/pages/Direccion.js
+++ src/pages/Direccion.js
@@ -1,24 +1,43 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import '../css/basic.css';
+import useLocalStorage from '../util/useLocalStorage';
 
+const client = axios.create({
+    baseURL: 'http://localhost:8080',
+});
 
 const Direccion = () => {
+    const [token, setToken] = useLocalStorage('', 'token');
+
     const [estado, setEstado] = useState();
     const [municipio, setMunicipio] = useState();
     const [cp, setCP] = useState();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        // const tokenUser = await loginUser({
-        //     username,
-        //     password,
-        // });
-
         console.log(estado, municipio, cp);
-        // if (tokenUser) {
-        //     setToken(tokenUser);
-        //     window.location.href = 'direccion';
-        // }
+        const result = await updateDireccion({ estado, municipio, cp });
+        console.log(result);
+    };
+
+    const updateDireccion = async (props) => {
+        try {
+            console.log(token);
+            const request = await client.post('/api/direccionUsuario', props, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            if (request.status === 201) {
+                console.log(request);
+                return request.data;
+            } else {
+                throw Error('Error');
+            }
+        } catch (error) {
+            console.error(error);
+        }
     };
 
     return (
